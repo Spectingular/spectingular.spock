4da8cb563974fa8bb1ea61df5512af94597c0d17
Mischa Dasberg <mischa@dasberg.nl>
2015-01-27 11:51:23 +0100

    Initial setup

---
+++ src/main/java/org/spectingular/spock/Spock.java
@@ -0,0 +1,34 @@
+package org.spectingular.spock;
+
+import org.slf4j.Logger;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Spock.
+ */
+@SpringBootApplication
+@PropertySource("classpath:/deck-config.properties")
+public class Spock extends SpringBootServletInitializer {
+    private static final Logger LOG = getLogger(Spock.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        LOG.info("Initializing Spock");
+        return application.sources(Spock.class);
+    }
+
+    /**
+     * Main.
+     * @param args The arguments.
+     */
+    public static void main(String... args) {
+        new Spock().configure(new SpringApplicationBuilder().showBanner(false)).run(args);
+    }
+
+
+}
