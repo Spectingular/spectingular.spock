964ef897e01402b75fcb622bf5c2226305006606
Mischa Dasberg <mischa@dasberg.nl>
2015-01-31 11:27:55 +0100

    Added Module to Phase

--- src/main/java/org/spectingular/spock/domain/Phase.java
+++ src/main/java/org/spectingular/spock/domain/Phase.java
@@ -26,6 +26,9 @@ public class Phase {
     @DBRef
     @JsonIgnore
     private Build build;
+    @DBRef
+    @JsonIgnore
+    private Module module;
 
     
     /**
@@ -61,18 +64,34 @@ public class Phase {
     }
 
     /**
-     * Gets the build.
-     * @return build The {@link Build}.
+     * Gets the {@link org.spectingular.spock.domain.Build}.
+     * @return build The {@link org.spectingular.spock.domain.Build}.
      */
     public Build getBuild() {
         return build;
     }
 
     /**
-     * Sets the {@link Build}.
-     * @param build The {@link Build}.
+     * Sets the {@link org.spectingular.spock.domain.Build}.
+     * @param build The {@link org.spectingular.spock.domain.Build}.
      */
     public void setBuild(final Build build) {
         this.build = build;
+    }
+
+    /**
+     * Gets the {@link org.spectingular.spock.domain.Module}. 
+     * @return module The {@link org.spectingular.spock.domain.Module}.
+     */
+    public Module getModule() {
+        return module;
+    }
+
+    /**
+     * Sets the {@link org.spectingular.spock.domain.Module}.
+     * @param module The {@link org.spectingular.spock.domain.Module}.
+     */
+    public void setModule(final Module module) {
+        this.module = module;
     }
 }
